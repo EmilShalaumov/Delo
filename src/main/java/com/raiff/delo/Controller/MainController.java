cdd7923a10bd3368bc5e823cefcb5fb379f1eeda
darkwolf999 <darkwolf999@mail.ru>
2019-09-14 15:40:06 +0300

    Implement database connection to hostland

--- src/main/java/com/raiff/delo/Controller/MainController.java
+++ src/main/java/com/raiff/delo/Controller/MainController.java
@@ -19,10 +19,11 @@ public class MainController {
     private UserRepository userRepository;
 
     @PostMapping(path="/add")
-    public @ResponseBody String addNewUser (@RequestParam String name , @RequestParam String email) {
+    public @ResponseBody String addNewUser (@RequestParam String name , @RequestParam String email, @RequestParam String password) {
         User n = new User();
         n.setName(name);
         n.setEmail(email);
+        n.setPassword(password);
         userRepository.save(n);
         return "Saved";
     }
