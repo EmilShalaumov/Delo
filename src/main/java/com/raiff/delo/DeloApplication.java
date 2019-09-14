617ede3a150ed54a89e464adcdda233106287ca7
Emil Shalaumov <emil.shalaumov@gmail.com>
2019-09-14 14:12:05 +0300

    Inital commit

---
+++ src/main/java/com/raiff/delo/DeloApplication.java
@@ -0,0 +1,13 @@
+package com.raiff.delo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raiff/delo/DeloApplicationTests.java
@@ -0,0 +1,16 @@
+package com.raiff.delo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeloApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
